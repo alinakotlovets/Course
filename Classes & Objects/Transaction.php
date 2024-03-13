16096a3054331d22b5e5c4d6713ae800c543cbbb
alinakotlovets <a.kotlovets@lntu.edu.ua>
2024-03-13 16:04:32 +0200

    comit

---
+++ Classes & Objects/Transaction.php
@@ -0,0 +1,35 @@
+<?php
+
+class Transaction
+{   private float $amount;
+    private string $description;
+    public function __construct( float $amount, string $description)
+    {
+        $this -> amount =$amount;
+
+        $this -> description = $description;
+
+    }
+
+    public function addTax(float $rate): Transaction
+    {
+        $this->amount +=  $this-> amount *  $rate/100;
+        return  $this;
+    }
+
+
+    public function gr(float $rate) : Transaction
+    {
+        $this->amount -=  $this -> amount *  $rate/100;
+        return  $this;
+    }
+    public function getAmount() : float
+    {
+    return $this->amount;
+    }
+    public function __destruct()
+    {
+        echo 'Destruct ' .  $this->description .'<br/>';
+    }
+
+}
